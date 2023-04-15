2ffed17841df9b792676ae9baf82e80f1a05c512
jbraden <jbraden@celestialmediagroupllc.com>
2023-04-14 22:46:24 -0500

    Need help from invalid login

--- login_page.py
+++ login_page.py
@@ -18,7 +18,7 @@ def test_successful_login():
     btn_log_in = driver.find_element(by=By.ID, value="logIn")
 
     txtfld_email.send_keys("jbraden@protonmail.com")
-    txtfld_password.send_keys("techInterview1")
+    txtfld_password.send_keys("")
     btn_log_in.click()
 
     time.sleep(5)
@@ -66,7 +66,6 @@ def test_hudl_logo_goes_to_main_page():
     assert title == "Log In"
 
     svg_hudl_logo = driver.find_element(by=By.CLASS_NAME, value="styles_hudlLogoContainer_1L8Lig-sH69T84pB_fXSlv")
-
     svg_hudl_logo.click()
 
     time.sleep(5)
@@ -88,7 +87,6 @@ def test_left_arrow_goes_to_main_page():
     assert title == "Log In"
 
     link_left_arrow = driver.find_element(by=By.CLASS_NAME, value="styles_backIconContainer_MhkioW9m8rx70X7CIGuws")
-
     link_left_arrow.click()
 
     time.sleep(5)
@@ -110,7 +108,33 @@ def test_need_help_goes_to_help_page():
     assert title == "Log In"
 
     link_need_help = driver.find_element(by=By.LINK_TEXT, value="Need help?")
+    link_need_help.click()
 
+    time.sleep(5)
+
+    txt_login_help = driver.find_element(by=By.XPATH, value='//h2[@data-qa-id="login-help-headline"]')
+
+    assert txt_login_help.is_displayed()
+
+    driver.close()
+    driver.quit()
+
+def test_need_help_from_invalid_log_in_goes_to_help_page():
+    driver = webdriver.Chrome(executable_path= "drivers/chromedriver.exe")
+
+    driver.get("https://www.hudl.com/login")
+
+    time.sleep(5)
+
+    title = driver.title
+    assert title == "Log In"
+
+    btn_log_in = driver.find_element(by=By.ID, value="logIn")
+    btn_log_in.click()
+
+    time.sleep(2)
+
+    link_need_help = driver.find_element(by=By.CLASS_NAME, value="uni-margin--quarter--left")
     link_need_help.click()
 
     time.sleep(5)
